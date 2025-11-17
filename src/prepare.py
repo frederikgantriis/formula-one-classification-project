0b1e608bb358e77946e3ad31401451a33cd8f049
Frederik Gantriis Møller <frmo24042@gmail.com>
2025-11-17 10:06:38 +0100

    chore(prepare): change from v12 to v11

--- src/prepare.py
+++ src/prepare.py
@@ -6,7 +6,7 @@ def main():
     rf = Roboflow(api_key=ROBOFLOWKEY)
     project = rf.workspace("main-803cc").project("f1-car-recognition-6b5om")
     version = project.version(1)
-    dataset = version.download("yolov12")
+    dataset = version.download("yolov11")
 
 
 if __name__ == "__main__":
